b98a0509cdb36601c5eea31d4b7af5fa3220b5dc
TBalo <tbalog426@gmail.com>
2022-10-29 23:48:08 +0100

    add files

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBoobWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories {get; set;}
+}
+}
